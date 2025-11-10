b03d1329fd0037e7319d794c12a7401d8e2f050d
Giovanni Ricca <giovanniricca@proton.me>
2025-11-10 12:59:29 +0100

    miuicamera-lisa: Use soong to copy camera configs
    Change-Id: I58b76c05b64a349a5acf253dab3b0184f7021f4c

--- vendor/vendor-vendor.mk
+++ vendor/vendor-vendor.mk
@@ -5,13 +5,11 @@
 PRODUCT_SOONG_NAMESPACES += \
     vendor/xiaomi/miuicamera-lisa/vendor
 
-PRODUCT_COPY_FILES += \
-    vendor/xiaomi/miuicamera-lisa/vendor/proprietary/vendor/etc/camera/camera_cnf.txt:$(TARGET_COPY_OUT_VENDOR)/etc/camera/camera_cnf.txt \
-    vendor/xiaomi/miuicamera-lisa/vendor/proprietary/vendor/etc/camera/st_license.lic:$(TARGET_COPY_OUT_VENDOR)/etc/camera/st_license.lic
-
 PRODUCT_PACKAGES += \
     libcamera_algoup_jni.xiaomi \
     libcamera_mianode_jni.xiaomi \
     libmicampostproc_client \
     vendor.xiaomi.hardware.campostproc@1.0_system \
-    MiuiCamera
+    MiuiCamera \
+    camera_cnf.txt \
+    st_license.lic
